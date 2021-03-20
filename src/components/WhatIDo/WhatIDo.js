66806f689e659ae7bd186eec3d930e14744300d7
Christian <christian.huang.49@gmail.com>
2021-03-20 16:00:52 +0100

    MOD : Fix

--- src/components/WhatIDo/WhatIDo.js
+++ src/components/WhatIDo/WhatIDo.js
@@ -5,7 +5,37 @@ export default function WhatIDo () {
   return (
     <div>
       <div className='WhatIDo'>
-        Coucou
+        <div className='container flex-center'>
+          <div className='title'>
+            What i do
+          </div>
+          <div className='row w-100'>
+            <div className='col-4'>
+              <i className='fa fa-laptop' />
+              Coucou
+            </div>
+            <div className='col-4'>
+              <i className='fa fa-object-group' />
+              Coucou
+            </div>
+            <div className='col-4'>
+              <i className='fa fa-paint-brush' />
+              Coucou
+            </div>
+            <div className='col-4'>
+              <i className='fa fa-mobile' />
+              Coucou
+            </div>
+            <div className='col-4'>
+              <i className='fa fa-first-order' />
+              Coucou
+            </div>
+            <div className='col-4'>
+              <i className='fa fa-clone' />
+              Coucou
+            </div>
+          </div>
+        </div>
       </div>
       <div className='test' />
     </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/js/dist/util'
+import 'font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
